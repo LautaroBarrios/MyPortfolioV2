dba8050de08923ecd3eed270422cc8126464f01e
Lautaro <barrios.g.lautaro@gmail.com>
2024-09-03 19:28:50 -0300

    feat: h- technology

--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -3,8 +3,8 @@ import { CardTechnology } from "./Card.jsx";
 
 const Technologies = () => {
   return (
-    <section className="flex bg-[#3d5357] w-full h-[22vh]  justify-center items-center">
-      <section className="flex flex-col bg-[#052226] w-full h-[20vh] py-4 justify-center items-center relative">
+    <section className="flex bg-[#3d5357] w-full h-[26vh]  justify-center items-center">
+      <section className="flex flex-col bg-[#052226] w-full h-[24vh] py-4 justify-center items-center relative">
         <div className="overflow-hidden w-full">
           <div className="flex whitespace-nowrap animate-scroll">
             {[...technologies, ...technologies].map((Element, index) => (
