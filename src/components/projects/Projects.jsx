a3a9e20dc557f2c3194ff8f20e4636ab1ae89dbe
Lautaro <barrios.g.lautaro@gmail.com>
2024-09-27 12:27:44 -0300

    fix: iframe scroll

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,30 @@
+import { useEffect } from "react";
+
 const Projects = () => {
+  useEffect(() => {
+    const iframe = document.getElementById("Editor");
+
+    const disableScroll = (e) => {
+      // Evita que la página haga scroll mientras se interactúa en el iframe
+      document.body.style.overflow = "hidden";
+    };
+
+    const enableScroll = (e) => {
+      // Vuelve a habilitar el scroll de la página
+      document.body.style.overflow = "";
+    };
+
+    // Añade los listeners para los eventos táctiles
+    iframe.contentWindow?.addEventListener("touchstart", disableScroll);
+    iframe.contentWindow?.addEventListener("touchend", enableScroll);
+
+    // Cleanup: Remueve los event listeners cuando el componente se desmonte
+    return () => {
+      iframe.contentWindow?.removeEventListener("touchstart", disableScroll);
+      iframe.contentWindow?.removeEventListener("touchend", enableScroll);
+    };
+  }, []);
+
   return (
     <section className="flex bg-[#072e33] w-full h-screen items-center justify-center">
       <iframe
@@ -10,6 +36,6 @@ const Projects = () => {
       ></iframe>
     </section>
   );
-}
+};
 
-export default Projects
+export default Projects;
