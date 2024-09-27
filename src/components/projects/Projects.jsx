ed89e79c060637f0cb226ed72d2603efb58f2604
Lautaro <barrios.g.lautaro@gmail.com>
2024-09-27 12:17:13 -0300

    feat: iframe

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,13 @@
 const Projects = () => {
   return (
     <section className="flex bg-[#072e33] w-full h-screen items-center justify-center">
-      <p>Projects</p>
+      <iframe
+        id="Editor"
+        width="96%"
+        height="90%"
+        className="rounded"
+        src="https://image-editor-three-wine.vercel.app/"
+      ></iframe>
     </section>
   );
 }
