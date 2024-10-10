8cb68724b1551df44927e298e57b35ae6ab722d6
Lautaro <barrios.g.lautaro@gmail.com>
2024-10-10 14:52:16 -0300

    fix: overflow

--- src/components/projects/layouts/Private.jsx
+++ src/components/projects/layouts/Private.jsx
@@ -2,13 +2,13 @@ const Private = () => {
   return (
     <section className="flex flex-col justify-center items-center h-[80%] w-full space-y-2 p-2 z-10 text-center text-white">
       <h5 className="text-2xl font-bold text-red-50">IMPORTANTE</h5>
-      <p className="text-xl font-bold border-b pb-4">
+      <p className="text-xl font-bold border-b pb-4 mx-4">
         Debido a que los siguientes proyectos manejan información
         delicada/privada no hay links de acceso disponibles para el público.
       </p>
-      <ol className="pt-4 w-2/3 space-y-2">
+      <ol className="pt-4 px-2 lg:w-2/3 w-5/6 space-y-2 lg:h-auto h-48 lg:overflow-visible overflow-auto">
         <li>
-          <p className=" text-lg">
+          <p className="text-[#6ed1d6] text-lg">
             Sistema de gestión de empleados y contratos - Municipalidad de la
             Calera, Córdoba, Argentina (2022).
           </p>
@@ -22,17 +22,18 @@ const Private = () => {
           </p>
         </li>
         <li>
-          <p className=" text-lg">
+          <p className="text-[#6ed1d6] text-lg">
             Sistema de análisis veterinario - (2023).
           </p>
           <p className="text-gray-300">
             Desarrollo de un sistema que permite la carga de los datos de
-            análisis para veterinarias, pudiendo generar PDFs con la
-            información del análisis, permitiendo a las veterinarias tener un
-            documento con los datos necesarios del análisis del paciente en un orden, formato accesible y
-            cómodo para las mismas. En este desarrollo pude aprender nuevos
-            conceptos, nuevas herramientas como React-PDF, pulir métodos y
-            herramientas de trabajo, además de superar las expectativas del cliente.
+            análisis para veterinarias, pudiendo generar PDFs con la información
+            del análisis, permitiendo a las veterinarias tener un documento con
+            los datos necesarios del análisis del paciente en un orden, formato
+            accesible y cómodo para las mismas. En este desarrollo pude aprender
+            nuevos conceptos, nuevas herramientas como React-PDF, pulir métodos
+            y herramientas de trabajo, además de superar las expectativas del
+            cliente.
           </p>
         </li>
       </ol>
